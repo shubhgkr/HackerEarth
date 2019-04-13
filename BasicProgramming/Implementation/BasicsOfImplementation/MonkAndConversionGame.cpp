4b4c6158b79bfcc4a91399e8fc7ebe99649cdaa7
shubhgkr <shubhgkr@gmail.com>
2019-04-14 01:22:35 +0530

    Monk And Conversion Game

---
+++ BasicProgramming/Implementation/BasicsOfImplementation/MonkAndConversionGame.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+int main(){
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	int t,n,i;
+	std::cin>>t;
+	while(t--){
+		std::cin>>n;
+		long long int x,sum=0;
+		for(i=0;i<n;i++){
+			std::cin>>x;
+			sum+=x;
+		}
+		for(i=0;i<n;i++){
+			std::cin>>x;
+			sum-=x;
+		}
+        std::cout<<(sum?"NO\n":"YES\n");
+	}
+	return 0;
+}
