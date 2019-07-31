3d661fee56a28b9a5467a47a601aac09367d2bf5
shubhgkr <shubhgkr@gmail.com>
2019-07-31 07:33:17 +0530

    N queen accepted solution. Using an another 1d vector 'result' to store the position of queen where it is placed.

--- Algorithms/Searching/Linear Search/nqueen.cpp
+++ Algorithms/Searching/Linear Search/nqueen.cpp
@@ -5,56 +5,50 @@
 #include<iostream>
 #include<vector>
 
-
-bool isValid(std::vector<std::vector<int>> board, int boardSize, int row, int col) {
-	//checks row or col
-	for (int i = 0; i < boardSize; i++)
-		if (board[row][i] || board[i][col])
-			return false;
-
-	//checks diagonals
-	for (int i = row - 1; i >= 0; --i) {
-		for (int j = col - 1; j >= 0; j--)
-			if (board[i][j])
-				return false;
-	}
-	for (int i = row - 1; i >= 0; --i) {
-		for (int j = col + 1; j < boardSize; j++)
-			if (board[i][j])
-				return false;
-	}
-	return true;
+bool isAttack(const std::vector<int> &queens, int x) {
+	int y = queens.size();
+	for (int i = 0; i < y; i++)
+		if (std::abs(queens[i] - x) == 0 || std::abs(queens[i] - x) == y - i)
+			return true;
+	return false;
 }
 
-bool NQueen(std::vector<std::vector<int>> &board, int boardSize, int N) {
-	if (N == 0)
-		return true;
+void queenSolution(const int boardSize, std::vector<int> queens, std::vector<std::vector<int>> &result) {
+	if (queens.size() >= boardSize) {
+		result.push_back(queens);
+		queens.clear();
+		return;
+	}
 	for (int i = 0; i < boardSize; i++) {
-		for (int j = 0; j < boardSize; j++) {
-			if (isValid(board, boardSize, i, j)) {
-				board[i][j] = 1;
-				if (NQueen(board, boardSize, N - 1))
-					return true;
-				board[i][j] = 0;
-			}
+		if (!isAttack(queens, i)) {
+			queens.push_back(i);
+			queenSolution(boardSize, queens, result);
+			queens.pop_back();
 		}
 	}
-	return false;
 }
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(nullptr);
-	int N;
-	std::cin >> N;
-	std::vector<std::vector<int>> board((N), std::vector<int>((N), 0));
-	if (NQueen(board, N, N)) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++)
-				std::cout << board[i][j] << " ";
+	int boardSize;
+	std::cin >> boardSize;
+	std::vector<int> queens;
+	std::vector<std::vector<int>> result;
+	queenSolution(boardSize, queens, result);
+	if (result.size() == 0)
+		std::cout << "Not possible";
+	else {
+		std::vector<int> ans = result[0];
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				if (ans[i] == j)
+					std::cout << "1 ";
+				else
+					std::cout << "0 ";
+			}
 			std::cout << "\n";
 		}
-	} else
-		std::cout << "Not possible\n";
+	}
 	return 0;
 }
